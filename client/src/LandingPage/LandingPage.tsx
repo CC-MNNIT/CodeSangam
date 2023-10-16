1dbd3f4012bc2f82f3403471ccaee2580755d9fc
imsubratpandey <imsubratpandey@gmail.com>
2023-10-17 02:10:41 +0530

    BG Position Fixed

--- client/src/LandingPage/LandingPage.tsx
+++ client/src/LandingPage/LandingPage.tsx
@@ -3,8 +3,8 @@ import background from "../Assets/background.gif";
 
 const LandingPage = () => {
     return (
-        <div className='text-white'>
-            <img src={background} style={{ width: "cal(100vw)" }} />
+        <div className='text-white h-screen w-screen overflow-hidden'>
+            <img src={background} />
         </div>
     )
 }
