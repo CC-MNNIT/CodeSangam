47bca5b116269357b4be5b1d3280c9cbec46ca85
Abhishek Patel <ap9506332677@gmail.com>
2023-10-17 18:27:16 +0530

    update

--- client/src/Components/LandingPage.tsx
+++ client/src/Components/LandingPage.tsx
@@ -5,13 +5,16 @@ import "./Styles.css"
 
 const LandingPage = () => {
     return (
-        <canvas id="main_canvas"></canvas>
-        // <div className='text-white text-center h-screen w-screen overflow-hidden'>
-        //     {/* <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'><NeonText text="CodeSangam" /></div> */}
-        //     <div className="logo absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">CodeSangam</div>
-        //     {/* <div className="logo absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-15vh text-white font-extralight font-comfortaa ">CodeSangam</div> */}
-        //     <img className="w-full h-auto" src={background} style={{ width: "cal(100vw)" }} />
-        // </div >
+        <div>
+            <canvas id="main_canvas">
+            </canvas>
+            <div className="logo absolute z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">CodeSangam</div>
+            {/* <div className='text-white text-center h-screen w-screen overflow-hidden'> */}
+            {/* <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'><NeonText text="CodeSangam" /></div> */}
+            {/* <div className="logo absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-15vh text-white font-extralight font-comfortaa ">CodeSangam</div> */}
+            {/* <img className="w-full h-auto" src={background} style={{ width: "cal(100vw)" }} /> */}
+            {/* </div > */}
+        </div >
     )
 }
 
