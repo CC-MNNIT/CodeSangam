86a564d3787976c417f94e547757135f41a90fe1
Abhishek Patel <ap9506332677@gmail.com>
2023-10-18 16:00:41 +0530

    moon png

--- client/src/Components/LandingPage.tsx
+++ client/src/Components/LandingPage.tsx
@@ -16,7 +16,7 @@ const LandingPage = () => {
             <canvas className='fixed' id="main_canvas">
             </canvas>
             {/* <div className="logo fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">CodeSangam</div> */}
-            <div style={{ opacity: topDivOpacity }} className="fixed logo"><b>CODE<span>SAN</span>GAM</b></div>
+            <div style={{ opacity: topDivOpacity }} className="fixed logo"><b>C<img className="rotate spinner" src='https://i.imgur.com/2Z3Svuj.png' alt="moon"></img>DE<span>SAN</span>GAM</b></div>
             {/* <div className='text-white text-center h-screen w-screen overflow-hidden'> */}
             {/* <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'><NeonText text="CodeSangam" /></div> */}
             {/* <div className="logo absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-15vh text-white font-extralight font-comfortaa ">CodeSangam</div> */}
