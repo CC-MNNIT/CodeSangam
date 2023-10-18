edcf168234a917c6ad618b72f623cbcf2b30f1a7
imsubratpandey@gmail.com <32bitdev@gmail.com>
2023-10-18 19:22:05 +0530

    Rotation Method Added

--- client/src/Components/LandingPage.tsx
+++ client/src/Components/LandingPage.tsx
@@ -5,6 +5,10 @@ import "./Styles.css"
 const LandingPage = () => {
 
     const [topDivOpacity, setTopDivOpacity] = useState(1);
+    const [angle, setAngle] = useState(0);
+    const rotateMoon = () => {
+        setAngle(angle + 72);
+    };
     window.addEventListener('scroll', () => {
         const currentScroll = window.scrollY;
         setTopDivOpacity(1 - currentScroll / 500);
@@ -12,11 +16,16 @@ const LandingPage = () => {
     });
     return (
         <div>
+            <button onClick={() => { rotateMoon(); }} style={{ zIndex: "50000", position: "fixed", color: "white" }}>Rotate(current angle - {angle})</button>
             <canvas className='fixed' id="main_canvas">
             </canvas>
             <div style={{ opacity: topDivOpacity }} className="fixed logo"><b>C<img className="rotate spinner" src='https://i.imgur.com/2Z3Svuj.png' alt="moon"></img>DE<span>SAN</span>GAM</b></div>
-            <div style={{overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", position: "relative", zIndex: 100, backgroundColor: "red", opacity: 0.5, height: "calc(200vh)", width: "calc(100vw)" }}>
-            <img style={{scale: "80%", transform: "translateY(1200px)"}} src={moon} alt="moon" />
+            <div style={{ overflow: "hidden", position: "relative", zIndex: 100, backgroundColor: "", opacity: 1, height: "calc(200vh+250px)", width: "calc(100vw)" }}>
+                <div style={{ overflow: "", display: "flex", justifyContent: "center", alignItems: "center", position: "relative", zIndex: 100, backgroundColor: "red", opacity: 0, height: "calc(200vh)", width: "calc(100vw)" }}>
+                </div>
+                <div style={{ overflow: "", display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "flex-start", position: "relative", zIndex: 100, backgroundColor: "", opacity: 1, height: "calc(250px)", width: "calc(100vw)" }}>
+                    <img style={{ position: "relative", top: "300px", transition: "transform 2s", transform: `rotate(${angle}deg)`, scale: "146%" }} src={moon} alt="moon" />
+                </div>
             </div>
         </div >
     )
