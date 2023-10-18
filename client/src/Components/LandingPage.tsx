77193c6cdc43a0f49ea85a8b12b6d40a7d7b4b0b
Abhishek Patel <ap9506332677@gmail.com>
2023-10-19 01:41:22 +0530

    card

--- client/src/Components/LandingPage.tsx
+++ client/src/Components/LandingPage.tsx
@@ -39,7 +39,7 @@ const LandingPage = () => {
         console.log("yy-" + height);
 
         const newScale = 200 * width / height;
-        console.log("scale-"+newScale);
+        console.log("scale-" + newScale);
         setScale(newScale);
 
     }, []);
@@ -52,45 +52,20 @@ const LandingPage = () => {
             <canvas className='fixed' id="main_canvas">
             </canvas>
             <div style={{ opacity: topDivOpacity }} className="fixed logo"><b>C<img className="rotate spinner" src='https://i.imgur.com/2Z3Svuj.png' alt="moon"></img>DE<span>SAN</span>GAM</b></div>
-
-
-
-
-            <div style={{ overflow: "hidden", height: "calc(190vh)", width: "100vw" }}>
-
-
-
-
-
-
+            <div style={{ overflow: "hidden", height: "calc(170vh)", width: "100vw" }}>
                 <div style={{ overflow: "hidden", position: "relative", height: "400vh", width: "100vw", backgroundColor: "", opacity: "1" }}>
-
-
-
                     <div style={{ width: "calc(100vw)", height: "calc(100vh)", transition: "transform 2s", transform: `rotate(${angle}deg)`, display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "", opacity: "1", scale: `${scale}%`, position: "absolute", bottom: "0px" }}>
                         <img style={{ position: "absolute", height: "100%" }} src={moon} alt="moon" />
                         <div id="container" style={{ position: "absolute", display: "flex", justifyContent: "center", alignItems: "center", height: "100%", zIndex: 20 }}>
-                            <div className="item">1</div>
-                            <div className="item">2</div>
-                            <div className="item">3</div>
-                            <div className="item">4</div>
-                            <div className="item">5</div>
+                            <div className="item rotate-[90deg]"></div>
+                            <div className="item rotate-[162deg]">2</div>
+                            <div className="item rotate-[-127deg]">3</div>
+                            <div className="item rotate-[-54deg]">4</div>
+                            <div className="item rotate-[18deg]">5</div>
                         </div>
                     </div>
-
-
-
                 </div>
-
-
-
-
-
             </div>
-
-
-
-
         </div >
     )
 }
