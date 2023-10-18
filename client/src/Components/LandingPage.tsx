5295f7e3c7f2244234c2a423d399cff701b29cd7
imsubratpandey@gmail.com <32bitdev@gmail.com>
2023-10-18 17:30:41 +0530

    Moon Added

--- client/src/Components/LandingPage.tsx
+++ client/src/Components/LandingPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import background from "../Assets/background.gif";
-import NeonText from "./NeonText";
+import moon from "../Assets/moon.svg";
 import "./Styles.css"
 
 const LandingPage = () => {
@@ -15,13 +14,10 @@ const LandingPage = () => {
         <div>
             <canvas className='fixed' id="main_canvas">
             </canvas>
-            {/* <div className="logo fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">CodeSangam</div> */}
             <div style={{ opacity: topDivOpacity }} className="fixed logo"><b>C<img className="rotate spinner" src='https://i.imgur.com/2Z3Svuj.png' alt="moon"></img>DE<span>SAN</span>GAM</b></div>
-            {/* <div className='text-white text-center h-screen w-screen overflow-hidden'> */}
-            {/* <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center'><NeonText text="CodeSangam" /></div> */}
-            {/* <div className="logo absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-15vh text-white font-extralight font-comfortaa ">CodeSangam</div> */}
-            {/* <img className="w-full h-auto" src={background} style={{ width: "cal(100vw)" }} /> */}
-            {/* </div > */}
+            <div style={{overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", position: "relative", zIndex: 100, backgroundColor: "red", opacity: 0.5, height: "calc(200vh)", width: "calc(100vw)" }}>
+            <img style={{scale: "80%", transform: "translateY(1200px)"}} src={moon} alt="moon" />
+            </div>
         </div >
     )
 }
