b1a181bb179f2763bf59c094034a27efacdab4de
imsubratpandey@gmail.com <32bitdev@gmail.com>
2023-10-20 22:33:15 +0530

    Leaderboard Added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Components/LandingPage";
 import Starfield from "./Components/Starfield";
 import NavBar from "./Components/NavBar";
+import Leaderboard from "./Components/Leaderboard";
+
 
 function App() {
     return (
-        <div className="">
-            <Starfield />
-            <LandingPage />
-            <NavBar />
-        </div>
-    );
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<div className=""><Starfield /><LandingPage /><NavBar /></div>} />
+                <Route path="/leaderboard" element={<Leaderboard />} />
+            </Routes>
+        </BrowserRouter >
+    )
 }
 
 export default App;
--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import moonwith from "../Assets/moon.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+    const navigate = useNavigate();
     return (
         <div>
             <nav className="bg-transparent fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
@@ -27,7 +29,7 @@ export default function NavBar() {
                                 <a href="#" className="card block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">EVENTS</a>
                             </li>
                             <li>
-                                <a href="#" className="card block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">LEADERBOARDS</a>
+                                <a onClick={() => navigate("/leaderboard")} className="card block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">LEADERBOARDS</a>
                             </li>
 
                         </ul>
