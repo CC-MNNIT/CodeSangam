57625dc0a1de65e526ae41b3baae4814f78387ca
32bitdev <32bitdev@gmail.com>
2023-10-25 14:22:56 +0530

    Stations Updated

--- client/src/Components/Stations.tsx
+++ client/src/Components/Stations.tsx
@@ -11,6 +11,7 @@ export default function Stations({ leftrotateMoon, rightrotateMoon }: props) {
 
     useEffect(() => {
         const script = document.createElement("script");
+        const { innerWidth: width, innerHeight: height } = window;
         script.async = true;
         script.innerHTML = `var factor=${factor},radius=Math.sqrt(screen.width*screen.width+factor*screen.height*factor*screen.height)/2,fields=$(".item"),container=$("#container"),width=container.width(),height=container.height(),angle=0,step=2*Math.PI/fields.length;fields.each(function(){var t=Math.round(width/2+radius*Math.cos(angle)-$(this).width()/2),i=Math.round(height/2+radius*Math.sin(angle)-$(this).height()/2);$(this).css({left:t+"px",top:i+"px"}),angle+=step});`;
         document.body.appendChild(script);
