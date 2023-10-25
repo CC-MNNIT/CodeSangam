a7ae56d0873269aa5578b58af34ad1ce586367bf
Shashank Verma <shashank.verma2002@gmail.com>
2023-10-25 15:52:40 +0530

    server: web: dashboard: Replace view notification functionality with logou
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- server/web/static/js/dashboard.js
+++ server/web/static/js/dashboard.js
@@ -1,41 +1,4 @@
-
-let active = 'nav-dashboard'
 let eventActive = 'event1'
-
-function setElementActive(elementId) {
-    let activeElement = document.getElementById(active)
-    if (activeElement) {
-        activeElement.classList.remove('bg-gray-900')
-        activeElement.classList.remove('text-white')
-        activeElement.classList.add('text-gray-300')
-        activeElement.classList.add('hover:bg-gray-700')
-        activeElement.classList.add('hover:text-white')
-    }
-
-    let activeElementMobile = document.getElementById(active + '-mb')
-    if (activeElementMobile) {
-        activeElementMobile.classList.remove('bg-gray-900')
-        activeElementMobile.classList.remove('text-white')
-        activeElementMobile.classList.add('text-gray-300')
-        activeElementMobile.classList.add('hover:bg-gray-700')
-        activeElementMobile.classList.add('hover:text-white')
-    }
-
-    let element = document.getElementById(elementId)
-    if (element) {
-        element.classList.add('bg-gray-900')
-        element.classList.add('text-white')
-    }
-
-    let elementMobile = document.getElementById(elementId + '-mb')
-    if (elementMobile) {
-        elementMobile.classList.add('bg-gray-900')
-        elementMobile.classList.add('text-white')
-    }
-    active = elementId
-
-    hideMenu()
-}
 
 function setEventActive(elementId) {
     // Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800"
@@ -77,14 +40,7 @@ function toggleMenu() {
         menu.classList.remove('hidden')
         menu.classList.add('block')
     } else {
-        hideMenu()
+        menu.classList.add('hidden')
+        menu.classList.remove('block')
     }
-}
-
-function hideMenu() {
-    let menu = document.getElementById('mobile-menu')
-    if (!menu) return
-
-    menu.classList.add('hidden')
-    menu.classList.remove('block')
 }
