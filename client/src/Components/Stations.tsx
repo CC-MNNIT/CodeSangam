8f0d2cc05e8e9bd0c0a7fd1c61f30c8b0ac6bb3f
32bitdev <32bitdev@gmail.com>
2023-10-28 21:50:50 +0530

    Station Error Fixed

--- client/src/Components/Stations.tsx
+++ client/src/Components/Stations.tsx
@@ -20,69 +20,69 @@ export default function Stations({ leftrotateMoon, rightrotateMoon }: props) {
     }, []);
 
     return (
-        <div id="container" style={{ position: "absolute" }}>
-            <div className="item" style={{ rotate: "89deg" }}>
-                1
-            </div>
-            <div className="item" style={{ rotate: "161deg" }}>
-                2
-            </div>
-            <div className="item" style={{ rotate: "-127deg" }}>
-                3
-            </div>
-            <div className="item" style={{ rotate: "-55deg" }}>
-                4
-            </div>
-            <div className="item" style={{ rotate: "17deg" }}>
-                5
-            </div>
-        </div>
         // <div id="container" style={{ position: "absolute" }}>
-        //     <div className="item sm:scale-50 md:scale-75 lg:scale-100" style={{ rotate: "89deg" }}>
-        //         <Cards
-        //             leftrotateMoon={leftrotateMoon}
-        //             rightrotateMoon={rightrotateMoon}
-        //             eventn="Logical Rhythm"
-        //             eventdescription="Logical Rythm is a machine learning event that empowers participants with essential knowledge and skills in data analysis, algorithm development, and model training. This event equips attendees with a strong foundation in artificial intelligence and data science, preparing them for exciting opportunities in these fields."
-        //             teamsize="Team Size: (1 - 3)"
-        //         />
+        //     <div className="item" style={{ rotate: "89deg" }}>
+        //         1
         //     </div>
-        //     <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "161deg" }}>
-        //         <Cards
-        //             leftrotateMoon={leftrotateMoon}
-        //             rightrotateMoon={rightrotateMoon}
-        //             eventn="Softablitz"
-        //             eventdescription="Softablitz is a Java Desktop Development event that immerses participants in the world of desktop application creation using the Java programming language. It provides hands-on experience, helping participants refine their Java skills while designing and developing powerful desktop software."
-        //             teamsize="Team Size: (1 - 3)"
-        //         />
+        //     <div className="item" style={{ rotate: "161deg" }}>
+        //         2
         //     </div>
-        //     <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "-127deg" }}>
-        //         <Cards
-        //             leftrotateMoon={leftrotateMoon}
-        //             rightrotateMoon={rightrotateMoon}
-        //             eventn="ContriHub"
-        //             eventdescription="Softablitz is a Java Desktop Development event that immerses participants in the world of desktop application creation using the Java programming language. It provides hands-on experience, helping participants refine their Java skills while designing and developing powerful desktop software."
-        //             teamsize="Team Size: 1"
-        //         />
+        //     <div className="item" style={{ rotate: "-127deg" }}>
+        //         3
         //     </div>
-        //     <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "-55deg" }}>
-        //         <Cards
-        //             leftrotateMoon={leftrotateMoon}
-        //             rightrotateMoon={rightrotateMoon}
-        //             eventn="Webster"
-        //             eventdescription="Webster is a web development-focused event, emphasizing the enhancement of participants' skills in creating web applications. It provides opportunities for individuals to learn about web technologies, frameworks, and best practices in the field."
-        //             teamsize="Team Size: (1 - 3)"
-        //         />
+        //     <div className="item" style={{ rotate: "-55deg" }}>
+        //         4
         //     </div>
-        //     <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "17deg" }}>
-        //         <Cards
-        //             leftrotateMoon={leftrotateMoon}
-        //             rightrotateMoon={rightrotateMoon}
-        //             eventn="Droidrush"
-        //             eventdescription="Droidrush is an event centered around Android development, where participants can delve into the world of Android app development. This event offers hands-on experience in building Android applications, exploring the Android ecosystem, and understanding mobile app design principles."
-        //             teamsize="Team Size: (1 - 3)"
-        //         />
+        //     <div className="item" style={{ rotate: "17deg" }}>
+        //         5
         //     </div>
         // </div>
+        <div id="container" style={{ position: "absolute" }}>
+            <div className="item sm:scale-50 md:scale-75 lg:scale-100" style={{ rotate: "89deg" }}>
+                <Cards
+                    leftrotateMoon={leftrotateMoon}
+                    rightrotateMoon={rightrotateMoon}
+                    eventn="Logical Rhythm"
+                    eventdescription="Logical Rythm is a machine learning event that empowers participants with essential knowledge and skills in data analysis, algorithm development, and model training. This event equips attendees with a strong foundation in artificial intelligence and data science, preparing them for exciting opportunities in these fields."
+                    teamsize="Team Size: (1 - 3)"
+                />
+            </div>
+            <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "161deg" }}>
+                <Cards
+                    leftrotateMoon={leftrotateMoon}
+                    rightrotateMoon={rightrotateMoon}
+                    eventn="Softablitz"
+                    eventdescription="Softablitz is a Java Desktop Development event that immerses participants in the world of desktop application creation using the Java programming language. It provides hands-on experience, helping participants refine their Java skills while designing and developing powerful desktop software."
+                    teamsize="Team Size: (1 - 3)"
+                />
+            </div>
+            <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "-127deg" }}>
+                <Cards
+                    leftrotateMoon={leftrotateMoon}
+                    rightrotateMoon={rightrotateMoon}
+                    eventn="ContriHub"
+                    eventdescription="Softablitz is a Java Desktop Development event that immerses participants in the world of desktop application creation using the Java programming language. It provides hands-on experience, helping participants refine their Java skills while designing and developing powerful desktop software."
+                    teamsize="Team Size: 1"
+                />
+            </div>
+            <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "-55deg" }}>
+                <Cards
+                    leftrotateMoon={leftrotateMoon}
+                    rightrotateMoon={rightrotateMoon}
+                    eventn="Webster"
+                    eventdescription="Webster is a web development-focused event, emphasizing the enhancement of participants' skills in creating web applications. It provides opportunities for individuals to learn about web technologies, frameworks, and best practices in the field."
+                    teamsize="Team Size: (1 - 3)"
+                />
+            </div>
+            <div className="item scale-50 md:scale-75 lg:scale-100" style={{ rotate: "17deg" }}>
+                <Cards
+                    leftrotateMoon={leftrotateMoon}
+                    rightrotateMoon={rightrotateMoon}
+                    eventn="Droidrush"
+                    eventdescription="Droidrush is an event centered around Android development, where participants can delve into the world of Android app development. This event offers hands-on experience in building Android applications, exploring the Android ecosystem, and understanding mobile app design principles."
+                    teamsize="Team Size: (1 - 3)"
+                />
+            </div>
+        </div>
     );
 }
