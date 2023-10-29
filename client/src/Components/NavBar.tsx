9a2368afaabaff1822fedf6125d9bf2b7eb09bd4
Abhishek Patel <ap9506332677@gmail.com>
2023-10-29 14:13:45 +0530

    navbar toggle added

--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -12,7 +12,7 @@ interface props {
 
 export default function NavBar({ isHome }: props) {
     const [logoOpacity, setLogoOpacity] = useState(0);
-    const [hide, setHide] = useState(true);
+    const [hide, setHide] = useState("none");
     // const [show, setShow] = useState(false);
     const navigate = useNavigate();
     window.addEventListener("scroll", () => {
@@ -29,14 +29,15 @@ export default function NavBar({ isHome }: props) {
                     </div>
                     <div className="flex md:order-2">
                         <button style={{ fontSize: "75%" }} onClick={() => navigate(`/${process.env.REACT_APP_BASE_URL}/api/auth`)} type="button" className="card text-blue-500 event-selected border-2 rounded-t-lg inline-block p-2 md:hover:text-blue-500">DASHBOARD</button>
-                        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center border-2 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none" aria-controls="navbar-sticky" aria-expanded="false">
+                        <button onClick={() => { hide == "" ? setHide("none") : setHide(""); }} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center border-2 p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                             </svg>
                         </button>
                     </div>
-                    <div style={{}} className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    {/* for desktop view */}
+                    <div style={{}} className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
                         <ul style={{ alignItems: "center" }} className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
                             <li>
                                 <div onClick={() => { navigate(`/${process.env.REACT_APP_BASE_URL}`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">HOME</div>
@@ -64,6 +65,38 @@ export default function NavBar({ isHome }: props) {
                             </li>
                             <li className='nav-mobile'>
                                 <div onClick={() => { navigate(`/${process.env.REACT_APP_BASE_URL}/leaderboard/logicalrhythm`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">LOGICAL RHYTHM RANKINGS</div>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* for mobile view */}
+                    <div style={{ display: hide }} className="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                        <ul style={{ alignItems: "center" }} className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+                            <li>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">HOME</div>
+                            </li>
+                            <li>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}`); window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">EVENTS</div>
+                            </li>
+                            <li className='nav-desktop'>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}/leaderboard/contrihub`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">LEADERBOARDS</div>
+                            </li>
+                            <li>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}/team`); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">TEAM</div>
+                            </li>
+                            <li className='nav-mobile'>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}/leaderboard/webster`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">WEBSTER RANKINGS</div>
+                            </li>
+                            <li className='nav-mobile'>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}/leaderboard/droidrush`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">DROIDRUSH RANKINGS</div>
+                            </li>
+                            <li className='nav-mobile'>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}/leaderboard/contrihub`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">CONTRIHUB RANKINGS</div>
+                            </li>
+                            <li className='nav-mobile'>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}/leaderboard/softablitz`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">SOFTABLITZ RANKINGS</div>
+                            </li>
+                            <li className='nav-mobile'>
+                                <div onClick={() => { setHide("none"); navigate(`/${process.env.REACT_APP_BASE_URL}/leaderboard/logicalrhythm`); window.scrollTo({ left: 0, top: 0 }); }} className="card block cursor-pointer py-2 pl-3 pr-4 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500">LOGICAL RHYTHM RANKINGS</div>
                             </li>
                         </ul>
                     </div>
