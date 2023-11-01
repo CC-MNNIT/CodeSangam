7c69a91f67fce57af313947c81522ad858153f6e
Shashank Verma <shashank.verma2002@gmail.com>
2023-11-02 02:01:55 +0530

    server: models: EventsInfo should not be cached
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- server/models/dto.go
+++ server/models/dto.go
@@ -52,52 +52,47 @@ type EventInfo struct {
 	Registered  bool
 }
 
-var eventsInfo []EventInfo = nil
-
 func GetEventsInfo(registered []bool) []EventInfo {
-	if eventsInfo == nil {
-		eventsInfo = []EventInfo{
-			{
-				Tag:         "droidrush",
-				Name:        "Droid Rush",
-				Tagline:     "The Android App Development Contest",
-				Description: "Droidrush is an event centered around Android development, where participants can delve into the world of Android app development. This event offers hands-on experience in building Android applications, exploring the Android ecosystem, and understanding mobile app design principles.",
-				TeamSize:    "1-3",
-				Tech:        "AndroidX, Jetpack Compose, Firebase, Kotlin, MVVM, Retrofit, Room, etc.",
-				Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
-				Registered:  registered[0],
-			},
-			{
-				Tag:         "webster",
-				Name:        "Webster",
-				Tagline:     "The Web Development Contest",
-				Description: "Webster is a web development-focused event, emphasizing the enhancement of participants' skills in creating web applications. It provides opportunities for individuals to learn about web technologies, frameworks, and best practices in the field.",
-				TeamSize:    "1-3",
-				Tech:        "HTML, CSS, JS, React, Next, Node, Express, MongoDB, etc.",
-				Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
-				Registered:  registered[1],
-			},
-			{
-				Tag:         "softablitz",
-				Name:        "Softablitz",
-				Tagline:     "The Software Development Contest",
-				Description: "Softablitz is a Java Desktop Development event that immerses participants in the world of desktop application creation using the Java programming language. It provides hands-on experience, helping participants refine their Java skills while designing and developing powerful desktop software.",
-				TeamSize:    "1-3",
-				Tech:        "JavaFX, Java, Swing, Desktop Compose, Kotlin, etc.",
-				Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
-				Registered:  registered[2],
-			},
-			{
-				Tag:         "logical",
-				Name:        "Logical Rhythm",
-				Tagline:     "The Competitive Coding Contest",
-				Description: "Logical Rythm is a machine learning event that empowers participants with essential knowledge and skills in data analysis, algorithm development, and model training. This event equips attendees with a strong foundation in artificial intelligence and data science, preparing them for exciting opportunities in these fields.",
-				TeamSize:    "1-3",
-				Tech:        "Jupyter Notebook, Kaggle, Google Colab, Python, Tensorflow, Keras, Scikitlearn, Pandas, Matplotlib, etc.",
-				Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
-				Registered:  registered[3],
-			},
-		}
+	return []EventInfo{
+		{
+			Tag:         "droidrush",
+			Name:        "Droid Rush",
+			Tagline:     "The Android App Development Contest",
+			Description: "Droidrush is an event centered around Android development, where participants can delve into the world of Android app development. This event offers hands-on experience in building Android applications, exploring the Android ecosystem, and understanding mobile app design principles.",
+			TeamSize:    "1-3",
+			Tech:        "AndroidX, Jetpack Compose, Firebase, Kotlin, MVVM, Retrofit, Room, etc.",
+			Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
+			Registered:  registered[0],
+		},
+		{
+			Tag:         "webster",
+			Name:        "Webster",
+			Tagline:     "The Web Development Contest",
+			Description: "Webster is a web development-focused event, emphasizing the enhancement of participants' skills in creating web applications. It provides opportunities for individuals to learn about web technologies, frameworks, and best practices in the field.",
+			TeamSize:    "1-3",
+			Tech:        "HTML, CSS, JS, React, Next, Node, Express, MongoDB, etc.",
+			Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
+			Registered:  registered[1],
+		},
+		{
+			Tag:         "softablitz",
+			Name:        "Softablitz",
+			Tagline:     "The Software Development Contest",
+			Description: "Softablitz is a Java Desktop Development event that immerses participants in the world of desktop application creation using the Java programming language. It provides hands-on experience, helping participants refine their Java skills while designing and developing powerful desktop software.",
+			TeamSize:    "1-3",
+			Tech:        "JavaFX, Java, Swing, Desktop Compose, Kotlin, etc.",
+			Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
+			Registered:  registered[2],
+		},
+		{
+			Tag:         "logical",
+			Name:        "Logical Rhythm",
+			Tagline:     "The Competitive Coding Contest",
+			Description: "Logical Rythm is a machine learning event that empowers participants with essential knowledge and skills in data analysis, algorithm development, and model training. This event equips attendees with a strong foundation in artificial intelligence and data science, preparing them for exciting opportunities in these fields.",
+			TeamSize:    "1-3",
+			Tech:        "Jupyter Notebook, Kaggle, Google Colab, Python, Tensorflow, Keras, Scikitlearn, Pandas, Matplotlib, etc.",
+			Rules:       "Make sure team name is creative and unique. Team name should not contain any offensive words, if found so, the team will be disqualified.",
+			Registered:  registered[3],
+		},
 	}
-	return eventsInfo
 }
